9bdd0586ddbec087b54bb99eaf1dd5156bda23f3
HIKman4949 <ikromkholov007@gmail.com>
2026-05-05 22:51:13 +0500

    Добавил вотрого игрока и создал две отдельные функции с лечением и атаки

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Hero struct {
 	Name    string
@@ -11,20 +15,26 @@ type Hero struct {
 }
 
 func NewHero(name string, damage int) *Hero {
-	return &Hero{
-		Name:    name,
-		HP:      100,
-		MaxHP:   100,
-		Damage:  damage,
-		IsAlive: true,
-	}
+	return &Hero{Name: name, HP: 100, MaxHP: 100, Damage: damage, IsAlive: true}
 }
 
 func (h *Hero) Attack(target *Hero) {
 	if !h.IsAlive || !target.IsAlive {
 		return
 	}
-	target.TakeDamage(h.Damage)
+
+	maxPossibleDamage := int(float64(target.HP) * 0.4)
+	if maxPossibleDamage < 5 {
+		maxPossibleDamage = 5
+	}
+
+	actualDamage := rand.Intn(h.Damage) + 1
+	if actualDamage > maxPossibleDamage {
+		actualDamage = maxPossibleDamage
+	}
+
+	fmt.Printf(" %s нанес %d урона %s\n", h.Name, actualDamage, target.Name)
+	target.TakeDamage(actualDamage)
 }
 
 func (h *Hero) TakeDamage(damage int) {
@@ -35,32 +45,30 @@ func (h *Hero) TakeDamage(damage int) {
 	}
 }
 
-func (h *Hero) Heal(amount int) {
-	if !h.IsAlive {
+func (h *Hero) Heal() {
+	if !h.IsAlive || h.HP >= h.MaxHP {
 		return
 	}
-	h.HP += amount
+
+	missingHP := h.MaxHP - h.HP
+	maxHeal := int(float64(missingHP) * 0.5)
+	if maxHeal < 1 {
+		maxHeal = 1
+	}
+
+	actualHeal := rand.Intn(maxHeal) + 5
+	h.HP += actualHeal
 	if h.HP > h.MaxHP {
 		h.HP = h.MaxHP
 	}
-}
-
-func (h *Hero) Status() {
-	if h.IsAlive {
-		fmt.Printf("[%s] HP: %d\n", h.Name, h.HP)
-	} else {
-		fmt.Printf("[%s] Повержен\n", h.Name)
-	}
+	fmt.Printf(" %s восстановил %d HP. Итого: %d\n", h.Name, actualHeal, h.HP)
 }
 
 func main() {
-	p1 := NewHero("Игрок 1", 25)
-	p2 := NewHero("Игрок 2", 20)
+	rand.Seed(time.Now().UnixNano())
+	p1 := NewHero("Гарри Поттер", 50)
+	p2 := NewHero("Питер Паркер", 50)
 
 	p1.Attack(p2)
-	p2.Attack(p1)
-	p1.Heal(10)
-
-	p1.Status()
-	p2.Status()
+	p2.Heal()
 }
